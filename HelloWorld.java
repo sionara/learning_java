a1941659baaa5f216669c2cea07ef3366c8b231c
sionara <35111239+sionara@users.noreply.github.com>
2019-08-30 11:32:52 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        method1();
+    }
+
+    public static void method1() {
+        System.out.println("My name is Sion!");
+    }
+}
+
